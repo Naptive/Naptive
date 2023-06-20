6d06fc08c8d277d7edf13e8052bfe532cbfec155
Naptive <10virtus21@gmail.com>
2023-06-21 00:35:33 +0200

    v3.2

--- components/Nav.js
+++ components/Nav.js
@@ -1,29 +1,8 @@
-import Link from "next/link";
-import  Image  from "../next.config";
-import { Router } from "next/router";
-
- const router = new Router();
- console.log(router);
-
-
 export default function Nav() {
   return (
-    <><nav>
-          <div className={styles.logo}>
-              <Image className={styles.LOG} src={logo} alt="Naptive logo" />
-          </div>
-      </nav><nav>
-              <div className={styles.NavLinks}>
-                  <Link href='/Contact' className={styles.link}>
-                      Contact
-                  </Link>
-                  <Link href='/Pricing' className={styles.link}>
-                      Pricing
-                  </Link>
-                  <Link href='/Team' className={styles.link}>
-                      Team
-                  </Link>
-              </div>
-          </nav></>
-  );
+    
+
+
+
+  )
 }
